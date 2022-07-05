aac18623ced0e919045e957ac2425ab8f1832d9e
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-07-05 04:25:51 +0400

    merge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-=======
-  content: ["./src/**/*.{html,js}"],
->>>>>>> 2c0fb835bb8ffb2ea1dfbb0fa14532b614d21730
   theme: {
     extend: {},
   },
