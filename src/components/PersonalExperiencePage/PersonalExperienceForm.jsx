a283495069af7fd1bf4cb852c482b88c7f589573
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-07-10 17:42:47 +0400

    validate personal form

--- src/components/PersonalExperiencePage/PersonalExperienceForm.jsx
+++ src/components/PersonalExperiencePage/PersonalExperienceForm.jsx
@@ -1,11 +1,99 @@
+import { useRef, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+
+import Next from '../../img/next-button.svg';
+
+
 const PersonalExperienceForm = () => {
+
+  const nameInputRef = useRef();
+  const emailInputRef = useRef();
+  const phoneNumberInputRef = useRef();
+  const birthDateInputRef = useRef();
+
+  const [isValid, setIsValid] = useState(false);
+
+  const validate = () => {
+
+    let validName = nameInputRef.current.value.length > 2;
+    let validEmail = emailInputRef.current.value.split('@')[1] === 'redberry.ge';
+
+    let regExp = /^\d+$/;
+    let validPhoneNumber = phoneNumberInputRef.current.value.length == 9 && regExp.test(phoneNumberInputRef.current.value);
+
+    // console.log(validName, validEmail, validPhoneNumber);
+
+    if (validName && validEmail && validPhoneNumber) {
+      setIsValid(true);
+    }
+
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPhoneNumber = phoneNumberInputRef.current.value;
+    const enteredBirthDate = birthDateInputRef.current.value;
+
+
+    console.log(`from personal page ${enteredName} ${enteredEmail} ${enteredPhoneNumber} ${enteredBirthDate}`);
+
+
+  };
+
+
   return (
     <div className="flex ml-16 mt-12">
-      <form className="w-full max-w-2xl">
-        <input type="text" id="name" required className="w-full h-12 my-5 p-5 text-xl border-b rounded-lg placeholder:text-redberryblack placeholder:hover:text-redberrypale hover:bg-redberrywhite  focus:bg-redberrygreysh outline-none placeholder:focus:text-redberryblack" placeholder="Name" />
-        <input type="email" id="email" required className="w-full h-12 my-5 p-5 text-xl border-b rounded-lg placeholder:text-redberryblack placeholder:hover:text-redberrypale hover:bg-redberrywhite  focus:bg-redberrygreysh outline-none placeholder:focus:text-redberryblack" placeholder="Email address" />
-        <input type="text" id="phone" required className="w-full h-12 my-5 p-5 text-xl border-b rounded-lg placeholder:text-redberryblack placeholder:hover:text-redberrypale hover:bg-redberrywhite  focus:bg-redberrygreysh outline-none placeholder:focus:text-redberryblack" placeholder="Phone number" />
-        <input type="date" id="date" required className="w-full h-12 my-5 p-5 text-xl border-b rounded-lg placeholder:text-redberryblack placeholder:hover:text-redberrypale hover:bg-redberrywhite  focus:bg-redberrygreysh focus:outline-none placeholder:focus:text-redberryblack" placeholder="Date of birth" />
+      <form className="w-full max-w-2xl" onSubmit={submitHandler}>
+        <input
+          className="w-full h-12 my-5 p-5 text-xl border-b rounded-lg placeholder:text-redberryblack placeholder:hover:text-redberrypale hover:bg-redberrywhite  focus:bg-redberrygreysh outline-none placeholder:focus:text-redberryblack"
+          type="text"
+          id="name"
+          required
+          placeholder="Name"
+          ref={nameInputRef}
+        />
+        <input
+          className="w-full h-12 my-5 p-5 text-xl border-b rounded-lg placeholder:text-redberryblack placeholder:hover:text-redberrypale hover:bg-redberrywhite  focus:bg-redberrygreysh outline-none placeholder:focus:text-redberryblack"
+          type="email"
+          id="email"
+          required
+          placeholder="Email address"
+          ref={emailInputRef}
+        />
+        <input
+          className="w-full h-12 my-5 p-5 text-xl border-b rounded-lg placeholder:text-redberryblack placeholder:hover:text-redberrypale hover:bg-redberrywhite  focus:bg-redberrygreysh outline-none placeholder:focus:text-redberryblack"
+          type="text"
+          id="phone"
+          required
+          placeholder="Phone number"
+          ref={phoneNumberInputRef}
+        />
+        <input
+          className="w-full h-12 my-5 p-5 text-xl border-b rounded-lg placeholder:text-redberryblack placeholder:hover:text-redberrypale hover:bg-redberrywhite  focus:bg-redberrygreysh focus:outline-none placeholder:focus:text-redberryblack"
+          type="date"
+          id="date"
+          required
+          placeholder="Date of birth"
+          ref={birthDateInputRef}
+        />
+        <div className='flex justify-between mt-20 max-w-2xl '>
+          <Link to='/'>
+            <button className='w-24 h-14 border-2 border-black rounded-xl hover:border-redberrypurple hover:bg-redberrygrey'>
+              Back
+            </button>
+          </Link>
+
+          {isValid && (
+            <Navigate to='/chess-experience' />
+          )}
+          <button onClick={validate}>
+            <img src={Next} alt="Get Started Right Arrow" className='hover:h-14 hover:border-4 hover:rounded-xl hover:border-redberrypurple' />
+          </button>
+        </div>
+
       </form>
     </div>
   );
--- src/components/PersonalExperiencePage/PersonalExperiencePageContent.jsx
+++ src/components/PersonalExperiencePage/PersonalExperiencePageContent.jsx
@@ -1,5 +1,3 @@
-import { Link } from 'react-router-dom';
-
 import LeftSide from '../Layout/LeftSide';
 import PersonalExperienceForm from './PersonalExperienceForm';
 
@@ -7,13 +5,8 @@ import Personal from '../../img/Personal.svg';
 import Header from '../Layout/Header';
 import ProgressBar from '../Layout/ProgressBar';
 import Title from '../Layout/Title';
-import Next from '../../img/next-button.svg';
 
 const PersonalExperiencePageContent = () => {
-
-  const test = () => {
-    console.log('from personal page');
-  };
 
   return (
     <div className="flex">
@@ -23,19 +16,6 @@ const PersonalExperiencePageContent = () => {
         <ProgressBar text={1} />
         <Title title='Personal Information' info='this is basic information fields' />
         <PersonalExperienceForm />
-        <div className='flex justify-between ml-20 mt-20 max-w-2xl '>
-          <Link to='/'>
-            <button className='w-24 h-14 border-2 border-black rounded-xl hover:border-redberrypurple hover:bg-redberrygrey'>
-              Back
-            </button>
-          </Link>
-
-          <Link to='/chess-experience'>
-            <button onClick={test}>
-              <img src={Next} alt="Get Started Right Arrow" className='hover:h-14 hover:border-4 hover:rounded-xl hover:border-redberrypurple' />
-            </button>
-          </Link>
-        </div>
       </div>
     </div>
 
